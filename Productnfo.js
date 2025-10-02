a5431b2bccb387fca8fc22c93e4d7661489da07b
Scrit1 <scrit1904@gmail.com>
2025-10-02 17:54:12 +0800

    Add files via upload

---
+++ Productnfo.js
@@ -0,0 +1,20 @@
+  createProduct(
+    "Cube Fidget",
+    "CubeFidget.webp",
+    "Cube fidget toy: 5x5 cube with movable small cubes, satisfying sound and feeling, useful when you want to have something to do when bored."
+  );
+  createProduct(
+    "Impossible passthrough",
+    "Impossible passthrough.webp",
+    "Impossible passthrough: A mindbending satisfaction to show off to people! "
+  );
+  createProduct(
+    "Quantum Skull",
+    "Quantum Skull.webp",
+    "Quantum Skull: An advanced and elaborate skull that is amazing and extremely cool!"
+  );
+  createProduct(
+    "Headphone stand",
+    "Headphone Stand.webp",
+    "Headphone stand: Need a place to store your headphones? This 3D print has got YOU covered!"
+  );
